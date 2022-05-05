f3044f71fba24b3ab9c72fbfcdbe43b096379884
MaxCojocari <92053176+MaxCojocari@users.noreply.github.com>
2022-05-05 19:52:22 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-
 import discord
 from discord.ui import Button, View
 import os
 from dotenv import load_dotenv
+import requests
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,21 +13,13 @@ bot = discord.Client(intents=intents)
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-
-def cyb_detector(text):
-    pass
-
-def spam_detector(text):
-    pass
-
+# change the generated URL here
+URL = "http://9865-34-80-27-204.ngrok.io"
 
 
 class PrimaryButton(Button):
     def __init__(self, label):
         super().__init__(label=label, style=discord.ButtonStyle.primary)
-    
-    async def callback(self, interaction):
-        await interaction.response.send_message(content="Type of cyberbullying:")
 
 
 class SuccessButton(Button):
@@ -38,72 +31,105 @@ class DangerButton(Button):
     def __init__(self, label):
         super().__init__(label=label, style=discord.ButtonStyle.danger)
 
-
-
-
-class MyView(View):
-    @discord.ui.button(label="Click here!", style=discord.ButtonStyle.green)
-    async def button_callback(self, button, interaction):
-        button1 = [x for x in self.children if x.custom_id == "danger"][0]
-        button1.disabled = True
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="Click here!", style=discord.ButtonStyle.red, custom_id="danger")
-    async def danger_button_callback(self, button, interaction):
-        await interaction.response.edit_message(view=self)
+cyb_count, spam_count, cyb_spam_count = 0, 0, 0
 
 @bot.event
 async def on_message(message):
+    global cyb_count, spam_count, cyb_spam_count
     if message.author == bot.user:
         return
 
     if message.content.startswith("$test"):
+        
+        data_json = {"text": message.content}
+        prediction = requests.get(URL, json=data_json).json()
 
-        is_cyberbullying = True #cyb_detector(message.content)
-        is_spam = True #spam_detector(message.content)
+        is_cyberbullying = int(prediction["cyberbullying"])
+        # if prediction["cyberbullying"]==1 and prediction["cyberbull_type"]['ethnicity']<0.5:
+        #     is_cyberbullying = 0
+        is_spam = int(prediction["spam"])
 
-        button1, button2 = SuccessButton("Yes"), DangerButton("No")
-        button = PrimaryButton("View Details")
+        button1, button2, button3 = SuccessButton("Yes"), DangerButton("No"), PrimaryButton("View Details")
         view = View()
 
         if is_cyberbullying and is_spam:
-            view.add_item(button)
-            await message.channel.send(f"{message.author.mention} This is both a spam and cyberbullying message!!!", reference=message, view=view)
+            cyb_spam_count += 1
+            view.add_item(button1)
+            view.add_item(button2)
+            view.add_item(button3)
+            await message.channel.send(
+                f"{message.author.mention} This is both a spam and cyberbullying message!!!" + 
+                f"\nI recommend you to delete this upsetting content. Continue deleting?", 
+                reference=message, 
+                view=view)
         
         elif is_cyberbullying:
-            view.add_item(button)
-            await message.channel.send(f"{message.author.mention} This is a cyberbullying message!!!", reference=message, view=view)
+            cyb_count += 1
+            view.add_item(button1)
+            view.add_item(button2)
+            view.add_item(button3)
+            await message.channel.send(
+                f"{message.author.mention} This is a cyberbullying message!!!" + 
+                f"\nI recommend you to delete this upsetting content. Continue deleting?", 
+                reference=message, 
+                view=view
+            )
         
         elif is_spam:  
-            await message.channel.send(f"{message.author.mention} Please, don't spam the channel!!!", reference=message, view=view)
+            spam_count += 1
+            await message.channel.send(
+                f"{message.author.mention} Please, don't spam the channel!!!", 
+                reference=message
+            )
+
+        else:
+            return
 
 
         async def button1_callback(interaction):
-            button1.disabled, button2.disabled = True, True
             await message.channel.send("The malicious message was successfully deleted!", reference=message)
+            button1.disabled, button2.disabled,  button3.disabled = True, True, True
             view.clear_items()
             await interaction.response.edit_message(view=view)
             await message.delete()
 
         async def button2_callback(interaction):
-            button1.disabled, button2.disabled = True, True
             await message.channel.send("Deletion canceled.\nWarning! This malicious message may affect emotionally other users.", reference=message)
-            view.clear_items()
+            button2.disabled = True
+            await interaction.response.edit_message(view=view)
+        
+        async def button3_callback(interaction):
+            embed = discord.Embed(
+                title="Type of cyberbullying:", 
+                url="https://www.stopbullying.gov/cyberbullying/what-is-it", 
+                description="Your message can be classified into following types (probabilities):", 
+                color=discord.Color.blue())
+            embed.set_thumbnail(url="https://media.istockphoto.com/vectors/cyber-bullying-people-vector-illustration-cartoon-flat-sad-young-vector-id1264371767?k=20&m=1264371767&s=612x612&w=0&h=H2_baTw3eW2rLoc-GcFubrasUDTJ5Ah5H5AcSsyW4rQ=")
+            embed.add_field(name="Age", value=prediction["cyberbull_type"]['age'], inline=True)
+            embed.add_field(name="Ethnicity", value=prediction["cyberbull_type"]['ethnicity'], inline=True)
+            embed.add_field(name="Gender", value=prediction["cyberbull_type"]['gender'], inline=True)
+            embed.add_field(name="Religion", value=prediction["cyberbull_type"]['religion'], inline=True)
+            button3.disabled = True
+            await message.channel.send(
+                reference=message,
+                embed=embed
+            )
             await interaction.response.edit_message(view=view)
         
             
-        button1.callback, button2.callback = button1_callback, button2_callback
+        button1.callback, button2.callback, button3.callback = button1_callback, button2_callback, button3_callback
 
 
         if is_spam and not is_cyberbullying:
             await message.channel.send("The malicious message was successfully deleted!", reference=message)
             await message.delete()
             return
-
-        view = View()
-        view.add_item(button1)
-        view.add_item(button2)
-        await message.channel.send(f"{message.author.mention} I recommend you to delete this upsetting content. Continue deleting?", reference=message, view=view)
+        
+        channel = bot.get_channel(971155287694274650)
+        if cyb_count + spam_count + cyb_spam_count == 3:
+            await channel.send(f"{message.author.mention} has already sent 3 malicious messages:"+
+            f"\n{cyb_count} cyberbullying\n{spam_count} spam\n {cyb_spam_count} both cyberbullying and spam")
+            cyb_count, spam_count, cyb_spam_count = 0, 0, 0
 
 
 @bot.event
